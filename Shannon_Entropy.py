36d1732aa0e8ef7c3b90833dde6fe028575a4988
xmaumd <xinyuanma521@gmail.com>
2018-10-02 23:15:31 -0400

    “”

---
+++ Shannon_Entropy.py
@@ -0,0 +1,29 @@
+def shannon_entropy(time_series):
+    """Return the Shannon Entropy of the sample data.
+    Args:
+        time_series: Vector or string of the sample data
+    Returns:
+        The Shannon Entropy as float value
+    """
+
+    # Check if string
+    if not isinstance(time_series, str):
+        time_series = list(time_series)
+
+    # Create a frequency data
+    data_set = list(set(time_series))
+    freq_list = []
+    for entry in data_set:
+        counter = 0.
+        for i in time_series:
+            if i == entry:
+                counter += 1
+        freq_list.append(float(counter) / len(time_series))
+
+    # Shannon entropy
+    ent = 0.0
+    for freq in freq_list:
+        ent += freq * np.log2(freq)
+    ent = -ent
+
+    return ent
