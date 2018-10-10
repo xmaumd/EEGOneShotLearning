d4025370e3157b2bf2d5a70dcf93742db0637ca4
Shawn <xmaumd@users.noreply.github.com>
2018-10-09 22:24:13 -0400

    Add files via upload

--- Process.py
+++ Process.py
@@ -3,26 +3,39 @@ import pandas as pd
 import numpy as np
 from pywt import wavedec
 
-file_name = 'chb02_16'
+#Traverse the files and get labels for each
+
+summary = pd.read_csv('C:/Users/Shawn Ma/Documents/2018Summer/CHBMIT dataset/02/csv/chb02_summary.csv')
+summary = summary.as_matrix()
+
+num = 16
+file_name = 'chb02_'+str(num)
 sig = pd.read_csv('C:/Users/Shawn Ma/Documents/2018Summer/CHBMIT dataset/02/csv/%s_data.csv' % file_name)
 sig = sig.as_matrix()
 sig = sig[:,1:]
 
+
+for entry in scandir('C:/Users/Shawn Ma/Documents/2018Summer/CHBMIT dataset/02/csv'):
+    print(entry)
+
+##To be refined
 #Slice into epochs
-epoch_len_in_secs = 3
-epoch_len = epoch_len_in_secs * 256
-num_epoch = int(np.floor(np.shape(sig)[0] / epoch_len))
-epoch_list = []
-for i in range(num_epoch):
-    epoch_list.append(sig[i*epoch_len:(i+1)*epoch_len,:])
+def slice(sig):
+    epoch_len_in_secs = 3
+    epoch_len = epoch_len_in_secs * 256
+    num_epoch = int(np.floor(np.shape(sig)[0] / epoch_len))
+    epoch_list = []
+    for i in range(num_epoch):
+        epoch_list.append(sig[i*epoch_len:(i+1)*epoch_len,:])
+    return(epoch_list)
 
 #Load the summary and label epochs
-summary = pd.read_csv('C:/Users/Shawn Ma/Documents/2018Summer/CHBMIT dataset/02/csv/chb02_summary.csv')
-summary = summary.as_matrix()
-loc = np.where(summary == file_name+'.edf')[0]
-sz_start = summary[loc,2]; sz_end = summary[loc,3]
-label = np.zeros([num_epoch,1])
-label[int(np.floor(float(sz_start)/3)):int(np.ceil(float(sz_end)/3))] = 1
+def label([summary, file_name, num_epoch]):
+    loc = np.where(summary == file_name+'.edf')[0]
+    sz_start = summary[loc,2]; sz_end = summary[loc,3]
+    label = np.zeros([num_epoch,1])
+    label[int(np.floor(float(sz_start)/3)):int(np.ceil(float(sz_end)/3))] = 1
+    return(label)
 
 freq = 256
 
